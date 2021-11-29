5d878b908481e5342b10cfef4449a2fd3d13ea3c
20A91A0586 /lalitha <81631254+La165@users.noreply.github.com>
2021-11-29 12:42:56 +0530

    Add files via upload

---
+++ opuni.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class sample
+{
+	private:
+		int x,y;
+    public:
+	sample()
+	{
+	x=10;
+	y=20;
+	}
+	void show()
+	{
+		cout<<"x="<<x<<endl;
+		cout<<"y="<<y<<endl;
+	}	
+	void operator -()	
+	{
+		x=-x;
+		y=-y;
+	}
+};
+int main()
+{
+sample s;
+-s;
+s.show();
+return 0;
+}
