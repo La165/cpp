121891d464aac008f5cc7dff9752a7a7c244492c
20A91A0586 /lalitha <81631254+La165@users.noreply.github.com>
2021-11-29 12:24:03 +0530

    Add files via upload

---
+++ lalfact.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+	if(n>1)
+	return n*factorial(n-1);
+	else
+	return 1;
+}
+int main()
+{
+	int n;
+	cout<<" enter the number:";
+	cin>>n;
+	cout<<"factorial value of "<<n<<"is "<<factorial(n);
+	return 0;
+	
+}
