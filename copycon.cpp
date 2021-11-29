bc6d0d13e47c6f9c721265530277cff22da0f1e8
20A91A0586 /lalitha <81631254+La165@users.noreply.github.com>
2021-11-29 12:19:56 +0530

    Add files via upload

---
+++ copycon.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class sample
+{
+	private:
+		int x;
+		public:
+			sample(sample &s)
+			{
+				x=s.x;
+			}
+			sample()
+			{
+				cout<<"enter x value:"<<endl;
+				cin>>x;
+			}
+			void show()
+			{
+				cout<<"x value is"<<x<<endl;
+			}
+};
+int main()
+{   
+    sample s1;
+	s1.show();
+	sample s2(s1);
+	s2.show();
+	return 0;
+}
