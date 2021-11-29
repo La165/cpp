bb9882bffcc3ecefe7955ad14f44705d4bf28136
20A91A0586 /lalitha <81631254+La165@users.noreply.github.com>
2021-11-29 12:25:48 +0530

    Add files via upload

---
+++ lasam2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	float b=23.564;
+	double c=896.567;
+	char d='l';
+	string e=" amma";
+	cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<< endl;
+	cout<<a<<endl<<b <<endl<<c<<endl<<d<<endl<<e;
+	return 0;
+}
