ddad749148864d0fedcf23d0f4f00a9db765e11b
20A91A0586 /lalitha <81631254+La165@users.noreply.github.com>
2021-11-29 12:21:41 +0530

    Add files via upload

---
+++ friendfn.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class base
+{
+	int val1,val2;
+	public:
+		void get()
+		{
+			cout<<"enter two values:";
+			cin>>val1>>val2;
+		}
+		friend float avg(base ob);
+};
+float avg(base ob)
+{
+	return float(ob.val1+ob.val2)/2;
+}
+int main()
+{
+	base obj;
+	obj.get();
+	cout<<"avg value is : "<<avg(obj)<<endl;
+	return 0;
+}
