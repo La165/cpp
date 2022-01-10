6dc4f97c98b9f830d47d063770ed7122c5db4c67
20A91A0586 /lalitha <81631254+La165@users.noreply.github.com>
2022-01-10 10:28:27 +0530

    Add files via upload

---
+++ deque.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int main()
+{
+	deque<int>deq;
+	for( int i=1;i<=5;++i)
+	{
+		deq.push_front(i);
+		deq.push_back(i*5);
+	}
+	deque<int>:: iterator d;
+	for(d=deq.begin();d!=deq.end();d++)
+	cout<<*d<<" ";
+	cout<<endl;
+	d=deq.begin();
+	++d;
+	deq.insert(d,1,34);
+	for(d=deq.begin();d!=deq.end();d++)
+	cout<<*d<<" ";
+	cout<<endl;
+	deq.pop_front();
+	deq.pop_back();
+	for(d=deq.begin();d!=deq.end();d++)
+	cout<<*d<<" ";cout<<endl;
+}
